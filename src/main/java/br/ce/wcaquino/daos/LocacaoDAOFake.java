3a4f199640615d553cb550ec47f07a6860902e58
EricRangelRJ <b.ericrangel@hotmail.com>
2021-10-16 13:22:39 -0300

    terceira commit teste

--- src/main/java/br/ce/wcaquino/daos/LocacaoDAOFake.java
+++ src/main/java/br/ce/wcaquino/daos/LocacaoDAOFake.java
@@ -2,6 +2,7 @@ package br.ce.wcaquino.daos;
 
 import br.ce.wcaquino.entidades.Locacao;
 
+//classe fake para teste
 public class LocacaoDAOFake implements LocacaoDAO {
 
 	public void salvar(Locacao locacao) {
